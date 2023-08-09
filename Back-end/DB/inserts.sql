6bcc67a86d91ba84a1c204348e3462b88e325c7e
Santiago Chimko <santiagochimko@gmail.com>
2023-08-09 08:54:37 -0300

    Cargo datos de artista/genero/album

--- Back-end/DB/inserts.sql
+++ Back-end/DB/inserts.sql
@@ -1,163 +1,97 @@
--- Insertar datos en la tabla "artistas" para el año 2020
+--@block
+-- Cargar datos en la tabla "artistas"
 INSERT INTO "artistas" ("Artista_id", "Nombre", "Genero", "FotoArtista")
 VALUES
-  (1, 'Bad Bunny', 'Reggaetón', 123456789),
-  (2, 'The Weeknd', 'Pop', 987654321),
-  (3, 'Dua Lipa', 'Pop', 567890123),
-  (4, 'BTS', 'K-pop', 456789012),
-  (5, 'Harry Styles', 'Pop', 345678901),
-  (6, 'Taylor Swift', 'Pop', 234567890),
-  (7, 'Post Malone', 'Hip-hop', 123456789),
-  (8, 'Billie Eilish', 'Pop', 987654321),
-  (9, 'Ariana Grande', 'Pop', 567890123),
-  (10, 'Drake', 'Hip-hop', 456789012);
+    (1, 'Beyoncé', 'R&B/Pop', 123456),
+    (2, 'Ed Sheeran', 'Pop', 789012),
+    (3, 'Rihanna', 'R&B/Pop', 345678),
+    (4, 'Michael Jackson', 'Pop', 901234),
+    (5, 'Taylor Swift', 'Pop/Country', 567890),
+    (6, 'Bruno Mars', 'Pop/R&B', 234567),
+    (7, 'Adele', 'Pop/Soul', 678901),
+    (8, 'Drake', 'Hip-Hop/R&B', 345678),
+    (9, 'Ariana Grande', 'Pop/R&B', 901234),
+    (10, 'Coldplay', 'Pop/Rock', 456789);
 
 --@block
-
--- Insertar álbumes ficticios para cada artista
--- Bad Bunny
+-- Cargar datos en la tabla "albums"
 INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
 VALUES
-  (1, 1, 'Álbum Ficticio 1 de Bad Bunny', 123456789),
-  (2, 1, 'Álbum Ficticio 2 de Bad Bunny', 987654321),
-  (3, 1, 'Álbum Ficticio 3 de Bad Bunny', 567890123);
-
--- The Weeknd
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (4, 2, 'Álbum Ficticio 1 de The Weeknd', 456789012),
-  (5, 2, 'Álbum Ficticio 2 de The Weeknd', 345678901),
-  (6, 2, 'Álbum Ficticio 3 de The Weeknd', 234567890);
-
--- Dua Lipa
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (7, 3, 'Álbum Ficticio 1 de Dua Lipa', 123456789),
-  (8, 3, 'Álbum Ficticio 2 de Dua Lipa', 987654321),
-  (9, 3, 'Álbum Ficticio 3 de Dua Lipa', 567890123);
-
--- BTS
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (10, 4, 'Álbum Ficticio 1 de BTS', 456789012),
-  (11, 4, 'Álbum Ficticio 2 de BTS', 345678901),
-  (12, 4, 'Álbum Ficticio 3 de BTS', 234567890);
-
--- Harry Styles
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (13, 5, 'Álbum Ficticio 1 de Harry Styles', 123456789),
-  (14, 5, 'Álbum Ficticio 2 de Harry Styles', 987654321),
-  (15, 5, 'Álbum Ficticio 3 de Harry Styles', 567890123);
-
--- Taylor Swift
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (16, 6, 'Álbum Ficticio 1 de Taylor Swift', 456789012),
-  (17, 6, 'Álbum Ficticio 2 de Taylor Swift', 345678901),
-  (18, 6, 'Álbum Ficticio 3 de Taylor Swift', 234567890);
-
--- Post Malone
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (19, 7, 'Álbum Ficticio 1 de Post Malone', 123456789),
-  (20, 7, 'Álbum Ficticio 2 de Post Malone', 987654321),
-  (21, 7, 'Álbum Ficticio 3 de Post Malone', 567890123);
-
--- Billie Eilish
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (22, 8, 'Álbum Ficticio 1 de Billie Eilish', 456789012),
-  (23, 8, 'Álbum Ficticio 2 de Billie Eilish', 345678901),
-  (24, 8, 'Álbum Ficticio 3 de Billie Eilish', 234567890);
-
--- Ariana Grande
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (25, 9, 'Álbum Ficticio 1 de Ariana Grande', 123456789),
-  (26, 9, 'Álbum Ficticio 2 de Ariana Grande', 987654321),
-  (27, 9, 'Álbum Ficticio 3 de Ariana Grande', 567890123);
-
--- Drake
-INSERT INTO "albums" ("Album_id", "Artista_id", "NombreAlbum", "FotoAlbum")
-VALUES
-  (28, 10, 'Álbum Ficticio 1 de Drake', 456789012),
-  (29, 10, 'Álbum Ficticio 2 de Drake', 345678901),
-  (30, 10, 'Álbum Ficticio 3 de Drake', 234567890);
+    (1, 1, 'Dangerously In Love', 123456),
+    (2, 1, 'I Am... Sasha Fierce', 789012),
+    (3, 1, 'Lemonade', 345678),
+    (4, 2, '÷', 901234),
+    (5, 2, 'x', 567890),
+    (6, 3, 'Good Girl Gone Bad', 234567),
+    (7, 3, 'Unapologetic', 678901),
+    (8, 3, 'Talk That Talk', 345678),
+    (9, 4, 'Thriller', 901234),
+    (10, 5, 'Fearless', 456789),
+    (11, 5, '1989', 123456),
+    (12, 6, 'Doo-Wops & Hooligans', 789012),
+    (13, 6, 'Uptown Special', 345678),
+    (14, 6, '24K Magic', 901234),
+    (15, 7, '21', 567890),
+    (16, 7, '25', 234567),
+    (17, 8, 'Views', 678901),
+    (18, 8, 'Scorpion', 345678),
+    (19, 9, 'My Everything', 901234),
+    (20, 9, 'Thank U, Next', 456789),
+    (21, 9, 'Positions', 123456),
+    (22, 10, 'Viva la Vida or Death and All His Friends', 789012),
+    (23, 10, 'X&Y', 345678),
+    (24, 10, 'Parachutes', 901234);
 
 --@block
-
--- Insertar canciones ficticias para cada artista
--- Bad Bunny
+-- Cargar datos en la tabla "canciones"
 INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
 VALUES
-  (1, 1, 1, 'Reggaetón', 'Canción Ficticia 1 de Bad Bunny'),
-  (2, 1, 1, 'Reggaetón', 'Canción Ficticia 2 de Bad Bunny'),
-  (3, 1, 1, 'Reggaetón', 'Canción Ficticia 3 de Bad Bunny');
+    -- Beyoncé
+    (1, 1, 1, 'R&B/Pop', 'Crazy in Love'),
+    (2, 1, 2, 'R&B/Pop', 'Single Ladies (Put a Ring on It)'),
+    (3, 1, 3, 'R&B/Pop', 'Formation'),
 
--- The Weeknd
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (4, 2, 2, 'Pop', 'Canción Ficticia 1 de The Weeknd'),
-  (5, 2, 2, 'Pop', 'Canción Ficticia 2 de The Weeknd'),
-  (6, 2, 2, 'Pop', 'Canción Ficticia 3 de The Weeknd');
+    -- Ed Sheeran
+    (4, 2, 4, 'Pop', 'Shape of You'),
+    (5, 2, 5, 'Pop', 'Thinking Out Loud'),
+    (6, 2, 4, 'Pop/Rock', 'Castle on the Hill'),
 
--- Dua Lipa
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (7, 3, 3, 'Pop', 'Canción Ficticia 1 de Dua Lipa'),
-  (8, 3, 3, 'Pop', 'Canción Ficticia 2 de Dua Lipa'),
-  (9, 3, 3, 'Pop', 'Canción Ficticia 3 de Dua Lipa');
+    -- Rihanna
+    (7, 3, 6, 'R&B/Pop', 'Umbrella'),
+    (8, 3, 7, 'R&B/Pop', 'Diamonds'),
+    (9, 3, 8, 'Electrónica/Dance', 'We Found Love'),
 
--- BTS
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (10, 4, 4, 'K-pop', 'Canción Ficticia 1 de BTS'),
-  (11, 4, 4, 'K-pop', 'Canción Ficticia 2 de BTS'),
-  (12, 4, 4, 'K-pop', 'Canción Ficticia 3 de BTS');
+    -- Michael Jackson
+    (10, 4, 9, 'Pop/R&B', 'Billie Jean'),
+    (11, 4, 9, 'Pop/Rock', 'Beat It'),
+    (12, 4, 9, 'Pop/Rock', 'Thriller'),
 
--- Harry Styles
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (13, 5, 5, 'Pop', 'Canción Ficticia 1 de Harry Styles'),
-  (14, 5, 5, 'Pop', 'Canción Ficticia 2 de Harry Styles'),
-  (15, 5, 5, 'Pop', 'Canción Ficticia 3 de Harry Styles');
+    -- Taylor Swift
+    (13, 5, 10, 'Pop/Country', 'Love Story'),
+    (14, 5, 11, 'Pop', 'Shake It Off'),
+    (15, 5, 11, 'Pop', 'Blank Space'),
 
--- Taylor Swift
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (16, 6, 6, 'Pop', 'Canción Ficticia 1 de Taylor Swift'),
-  (17, 6, 6, 'Pop', 'Canción Ficticia 2 de Taylor Swift'),
-  (18, 6, 6, 'Pop', 'Canción Ficticia 3 de Taylor Swift');
+    -- Bruno Mars
+    (16, 6, 12, 'Pop/R&B', 'Just the Way You Are'),
+    (17, 6, 13, 'Funk/Pop', 'Uptown Funk'),
+    (18, 6, 14, 'R&B/Pop/Funk', '24K Magic'),
 
--- Post Malone
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (19, 7, 7, 'Hip-hop', 'Canción Ficticia 1 de Post Malone'),
-  (20, 7, 7, 'Hip-hop', 'Canción Ficticia 2 de Post Malone'),
-  (21, 7, 7, 'Hip-hop', 'Canción Ficticia 3 de Post Malone');
+    -- Adele
+    (19, 7, 15, 'Pop/Soul', 'Rolling in the Deep'),
+    (20, 7, 15, 'Pop/Soul', 'Someone Like You'),
+    (21, 7, 16, 'Pop/Soul', 'Hello'),
 
--- Billie Eilish
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (22, 8, 8, 'Pop', 'Canción Ficticia 1 de Billie Eilish'),
-  (23, 8, 8, 'Pop', 'Canción Ficticia 2 de Billie Eilish'),
-  (24, 8, 8, 'Pop', 'Canción Ficticia 3 de Billie Eilish');
+    -- Drake
+    (22, 8, 17, 'Hip-Hop/R&B', 'Hotline Bling'),
+    (23, 8, 18, 'Hip-Hop/R&B', 'God´s Plan'),
+    (24, 8, 18, 'Hip-Hop/R&B', 'In My Feelings'),
 
--- Ariana Grande
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (25, 9, 9, 'Pop', 'Canción Ficticia 1 de Ariana Grande'),
-  (26, 9, 9, 'Pop', 'Canción Ficticia 2 de Ariana Grande'),
-  (27, 9, 9, 'Pop', 'Canción Ficticia 3 de Ariana Grande');
+    -- Ariana Grande
+    (25, 9, 19, 'Pop/R&B', 'Problem'),
+    (26, 9, 20, 'Pop/R&B', 'Thank U, Next'),
+    (27, 9, 21, 'Pop/R&B', 'Positions'),
 
--- Drake
-INSERT INTO "canciones" ("Cancion_id", "Artista_id", "Album_id", "Genero", "Nombre")
-VALUES
-  (28, 10, 10, 'Hip-hop', 'Canción Ficticia 1 de Drake'),
-  (29, 10, 10, 'Hip-hop', 'Canción Ficticia 2 de Drake'),
-  (30, 10, 10, 'Hip-hop', 'Canción Ficticia 3 de Drake');
-
-
-
-
+    -- Coldplay
+    (28, 10, 22, 'Pop/Rock', 'Viva la Vida'),
+    (29, 10, 23, 'Pop/Rock', 'Fix You'),
+    (30, 10, 24, 'Pop/Rock', 'Yellow');
