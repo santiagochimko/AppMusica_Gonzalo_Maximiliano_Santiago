2efe54fbffe239da38afe6fe5901d4eb7da4a5ee
Maximiliano <Maximiliano.Corbo394@gmail.com>
2023-08-09 15:33:15 -0300

    Correccion

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
-import matchScreen from './components/matchScreen'
+// import matchScreen from './components/matchScreen'
 import './App.css'
 import React from 'react';
 import Home from './views/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
-import React from 'react';
-import Home from './views/Home';
+// function App() {
+//   const [count, setCount] = useState(0)
+  
 
 const App = () => {
   return (
     <>
-      <matchScreen />
-
+      {/* <matchScreen /> */}
       <div className="app">
         <Home />
       </div>
