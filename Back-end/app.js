88ce7b489fb9b7b26fe145462b8df029c8597a40
Gonzalo Bacigalupe <gbs2790@gmail.com>
2023-08-11 10:41:05 -0300

    orden de carpetas y files, authmiddleware

--- Back-end/app.js
+++ Back-end/app.js
@@ -2,9 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3002;
 app.use(express.json());
-const routes = require("./routes");
-const { authMiddleWare } = require("./middleware");
-app.use("/api", routes);
+const { authMiddleWare } = require("./middleware/auth.middleware");
+const userRoutes = require("./routes/user.routes");
+const authroutes = require("./routes/auth.routes");
+const requestmiddle = require("./middleware/request.middleware");
+const {
+  initalLogging,
+  finalLogging,
+} = require("./middleware/loggin.middleware");
+
+app.use(requestmiddle);
+app.use(initalLogging);
+
+app.use("/user", authMiddleWare, userRoutes);
+app.use("/auth", authroutes);
+
+app.use(finalLogging);
 
 app.listen(port, () => {
   console.log(`Estas escuchando el puerto ${port}`);
