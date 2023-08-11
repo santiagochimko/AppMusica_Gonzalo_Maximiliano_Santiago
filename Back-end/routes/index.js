04031672a0f1828b26af54ebcf1d9abd1b064eb5
Gonzalo Bacigalupe <gbs2790@gmail.com>
2023-08-11 09:28:18 -0300

    login y register funcionando/ bd actualizada

--- Back-end/routes/index.js
+++ Back-end/routes/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const { traerCanciones, traerFiltros, login } = require("../controller");
+const {
+  traerCanciones,
+  traerFiltros,
+  login,
+  register,
+} = require("../controller");
 const routes = express.Router();
+const validaciones = require("../validators/user/user-validator");
+const runvalidation = require("../validators");
 
 //Traer canciones
 routes.get("/canciones", traerCanciones);
@@ -8,5 +15,7 @@ routes.get("/canciones", traerCanciones);
 routes.get("/filtros", traerFiltros);
 //Login
 routes.post("/login", login);
+//register
+routes.post("/register", validaciones, runvalidation, register);
 
 module.exports = routes;
