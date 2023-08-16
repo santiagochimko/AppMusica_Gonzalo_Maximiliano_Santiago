06c965a6e676a92fbe8437bebdb9d299a04a53ac
Gonzalo Bacigalupe <gbs2790@gmail.com>
2023-08-16 10:36:13 -0300

    endpoint listafiltros y endpoint artistas

--- Back-end/routes/user.routes.js
+++ Back-end/routes/user.routes.js
@@ -2,16 +2,21 @@ const express = require("express");
 const {
   traerCanciones,
   traerFiltros,
-  crearLista,
+  crearListaFiltros,
+  traerArtistas,
 } = require("../controller/user.controller");
 const userRoutes = express.Router();
 
 //Traer canciones
 userRoutes.get("/canciones", traerCanciones);
+
+//Traer artistas
+userRoutes.get("/artistas", traerArtistas);
+
 //Traer filtros
 userRoutes.get("/filtros", traerFiltros);
 
 //crear playlist
-userRoutes.post("/crearlista", crearLista);
+userRoutes.post("/crearlista", crearListaFiltros);
 
 module.exports = userRoutes;
